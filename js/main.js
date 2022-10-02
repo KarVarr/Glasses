cbac937f65096690347b3ff14ac6c7076da5a74e
Karen Vardanian <karenvardanian@MacBook-Air-Karen.local>
2022-10-02 12:33:02 +0300

    started the project

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.swiper-container', {
+  direction: 'vertical',
+  mousewheelControl: true,
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination',
+
+    clickable: true,
+  },
+ freeMode: true,
+  freeModeSticky: true,
+});
